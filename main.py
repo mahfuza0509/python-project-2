77e3416093c466d35c0dcdcb1a05bee59bb261cb
mahfuza0509 <baltabaevanina0@gmail.com>
2024-08-01 08:54:55 +0500

    python uyga vazifasi 'serkuyosh'

---
+++ main.py
@@ -0,0 +1,8 @@
+a = '''
+Serquyosh, hur oʻlkam, elga baxt, najot,
+Sen oʻzing doʻstlarga yoʻldosh, mehribon, mehribon!
+Yashnagay to abad ilmu fan, ijod,
+Shuhrating porlasin toki bor jahon!
+   '''
+b = a;
+print(b[0:10]);
